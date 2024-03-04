1e603a547525e2b8c01f5436579225f1e71ea744
otumian <popecan1000@gmail.com>
2024-03-04 13:50:18 +0000

    refactor th controller

--- src/main/kotlin/com/otumian/springbootapiwithvalidation/controller/ArticleController.kt
+++ src/main/kotlin/com/otumian/springbootapiwithvalidation/controller/ArticleController.kt
@@ -1,20 +1,13 @@
-package com.otumian.springbootapiwithvalidation
+package com.otumian.springbootapiwithvalidation.controller
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import com.otumian.springbootapiwithvalidation.dto.ArticleDto
+import com.otumian.springbootapiwithvalidation.service.ArticleService
 import org.springframework.web.bind.annotation.*
 
 
-data class ApiError(val message: String, val status: HttpStatus=HttpStatus.BAD_REQUEST)
-@RestControllerAdvice
 @RestController
 @RequestMapping("/api/v1/articles")
 class ArticleController(val service: ArticleService) {
-
-    @ExceptionHandler(Exception::class)
-    fun handleExceptions(exception: Exception): ResponseEntity<ApiError> {
-        return ResponseEntity(exception.message?.let { ApiError(it) }, HttpStatus.BAD_REQUEST)
-    }
 
     @GetMapping
     fun articles() = service.getArticles()
