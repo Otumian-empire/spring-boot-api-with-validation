3e4925fd6e17c63d213510fa75e3965707c9c415
otumian <popecan1000@gmail.com>
2024-03-05 04:58:26 +0000

    apply bean validation

--- src/main/kotlin/com/otumian/springbootapiwithvalidation/controller/ArticleController.kt
+++ src/main/kotlin/com/otumian/springbootapiwithvalidation/controller/ArticleController.kt
@@ -2,6 +2,11 @@ package com.otumian.springbootapiwithvalidation.controller
 
 import com.otumian.springbootapiwithvalidation.dto.ArticleDto
 import com.otumian.springbootapiwithvalidation.service.ArticleService
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+import org.springframework.format.annotation.NumberFormat
 import org.springframework.web.bind.annotation.*
 
 
@@ -16,11 +21,37 @@ class ArticleController(val service: ArticleService) {
     fun article(@PathVariable id: Long) = service.getArticleById(id)
 
     @PostMapping
-    fun newArticle(@RequestBody article: ArticleDto) = service.createArticle(article)
+    fun newArticle(@Valid @RequestBody article: ArticleDto) = service.createArticle(article)
 
     @PutMapping("/{id}")
-    fun updateArticle(@RequestBody article: ArticleDto, @PathVariable id: Long) = service.updateArticle(id, article)
+    fun updateArticle(
+        @Valid
+        @RequestBody article: ArticleDto,
+        @Valid
+        @PathVariable
+        @NotNull(message = "Id required")
+        @NumberFormat(style = NumberFormat.Style.NUMBER)
+        @Positive(message = "Id must be a positive number")
+        id: Long
+    ): ArticleDto? {
+
+        var a: ArticleDto? = null
+        try {
+         a=service.updateArticle(id, article)
+
+        } catch (exception: Exception) {
+            println(exception)
+        }
+        return  a
+    }
+
 
     @DeleteMapping("/{id}")
-    fun deleteArticle(@PathVariable id: Long) = service.deleteArticleById(id)
+    fun deleteArticle(
+        @Valid
+        @PathVariable
+        @NotNull(message = "Id required")
+        @Positive(message = "Id must be a positive number")
+        id: Long
+    ) = service.deleteArticleById(id)
 }
--- src/main/kotlin/com/otumian/springbootapiwithvalidation/dto/ArticleDto.kt
+++ src/main/kotlin/com/otumian/springbootapiwithvalidation/dto/ArticleDto.kt
@@ -1,12 +1,29 @@
 package com.otumian.springbootapiwithvalidation.dto
 
+import com.otumian.springbootapiwithvalidation.bean.validation.HasAdequateWords
 import com.otumian.springbootapiwithvalidation.extension.toSlug
+import jakarta.validation.constraints.NotBlank
 import java.time.LocalDateTime
+
 
 data class ArticleDto(
     var id: Long?,
-    var title: String,
-    var content: String,
-    val createdAt: LocalDateTime = LocalDateTime.now(),
-    val slug: String = title.toSlug()
+
+//    @field:NotNull(message = "title must not be null")
+//    @field:NotBlank(message = "title must not be blank")
+//    @field:NotEmpty(message = "title must not be empty")
+//    @field:HasAdequateWords(min = 3, max = 10, message = "title must be between 3 to 10 words")
+    @field:HasAdequateWords(message = "title must be at most 10 words")
+//    @field:Size(min = 3, max = 10, message = "title should have between 3 to a max of 10 words")
+    var title: String?,
+
+//    @field:NotNull(message = "content must not be null")
+//    @field:NotBlank(message = "content must not be blank")
+//    @field:NotEmpty(message = "content must not be empty")
+    @field:HasAdequateWords(min = 10, max = 25, message = "content must be between 10 to 25 words")
+//    @field:HasMaximumWord(message = "content must be at most 10 words")
+//    @field:Size(min = 10, max = 20, message = "content should have between 10 to a max of 20 words")
+    var content: String?,
+
+    val createdAt: LocalDateTime = LocalDateTime.now(), val slug: String? = title?.toSlug()
 )
