74691442fa2f3a8c6b094517b4ff7eff89aa9ffd
otumian <popecan1000@gmail.com>
2024-03-05 04:56:20 +0000

    format CreateArticleValidation

--- src/main/kotlin/com/otumian/springbootapiwithvalidation/validation/CreateArticleValidation.kt
+++ src/main/kotlin/com/otumian/springbootapiwithvalidation/validation/CreateArticleValidation.kt
@@ -9,24 +9,20 @@ interface ICreateArticleValidation {
 }
 
 @Service
-class CreateArticleValidation(val mechanism: IStringValidationMechanism): ICreateArticleValidation {
-   override fun validate(article: ArticleDto) {
-        this.titleValidation(article.title)
-        this.contentValidation(article.content)
+class CreateArticleValidation(val mechanism: IStringValidationMechanism) : ICreateArticleValidation {
+    override fun validate(article: ArticleDto) {
+        this.titleValidation(article.title!!)
+        this.contentValidation(article.content!!)
     }
 
     private fun titleValidation(title: String, minimum: Int = 3, maximum: Int = 10) {
         this.mechanism.isNotNull(title, "title is required")
         this.mechanism.isString(title, "title must be a string")
         this.mechanism.isAtLeast(
-            value = title,
-            minimum = minimum,
-            message = "title must be at least $minimum words"
+            value = title, minimum = minimum, message = "title must be at least $minimum words"
         )
         this.mechanism.isAtMost(
-            value = title,
-            maximum = maximum,
-            message = "title must be at most $maximum words"
+            value = title, maximum = maximum, message = "title must be at most $maximum words"
         )
     }
 
@@ -34,14 +30,10 @@ class CreateArticleValidation(val mechanism: IStringValidationMechanism): ICreat
         this.mechanism.isNotNull(content, "content is required")
         this.mechanism.isString(content, "content must be a string")
         this.mechanism.isAtLeast(
-            value = content,
-            minimum = minimum,
-            message = "content must be at least $minimum words"
+            value = content, minimum = minimum, message = "content must be at least $minimum words"
         )
         this.mechanism.isAtMost(
-            value = content,
-            maximum = maximum,
-            message = "content must be at most $maximum words"
+            value = content, maximum = maximum, message = "content must be at most $maximum words"
         )
     }
 }
